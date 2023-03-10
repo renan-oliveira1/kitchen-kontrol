27ae1057b12393bd5c978d6eec64b1ce39c02921
Renan Oliveira <renan.oliveira.rao@gmail.com>
2023-03-10 09:44:59 -0300

    Edit routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { KitchenComponent } from './components/kitchen/kitchen.component';
+import { BillComponent } from './components/bill/bill.component';
+import { ManagerComponent } from './components/manager/manager.component';
+import { HomeComponent } from './components/home/home.component';
+import { OrderPipelineComponent } from './components/order-pipeline/order-pipeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'order', component: OrderPipelineComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'manager', component: ManagerComponent},
+  {path: 'bill', component: BillComponent},
+  {path: 'kitchen', component: KitchenComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: '', redirectTo:'menu', pathMatch:'full' },
+  {path: '**', redirectTo:'menu', pathMatch:'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
